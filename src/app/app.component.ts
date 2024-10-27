9a5b8c24919e058c5d2070e166a6a89b06d1cb38
TranBaoTran <117694181+TranBaoTran@users.noreply.github.com>
2024-10-28 00:25:48 +0700

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, ProductListComponent, HeaderComponent],
+  imports: [RouterModule, ProductListComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
