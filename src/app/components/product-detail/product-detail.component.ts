071e8ed5b02e5cc501cc17f25a2fe07f09be7b30
Anhdeptroai <nguyentrunganh30092003@gmail.com>
2024-10-31 23:19:57 +0700

    update pruduct detail

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
@@ -8,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent {
+  product: any;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    if (history.state && history.state.product) {
+      this.product = history.state.product;
+    } else {
+      const productId = this.route.snapshot.paramMap.get('id');
+      // Tải dữ liệu từ server nếu không có trong `state`
+    }
+  }
 }
