26a8fb306c46248fee14dee05b83f53f95828e46
Anhdeptroai <nguyentrunganh30092003@gmail.com>
2024-11-06 22:50:19 +0700

    Lưu các thay đổi tạm thời

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,22 @@
 import { Routes } from '@angular/router';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { HomeComponent } from './components/home/home.component';
+
 
 export const routes: Routes = [
-    { path: '', component: ProductListComponent},
-    { path: 'category/:query', component: ProductListComponent}
+    {   path: '', 
+        component: HomeComponent,
+        children: [
+            { path: '', component: ProductListComponent },
+            { path: 'category/:query', component: ProductListComponent },
+            { path: 'product-detail/:id', component: ProductDetailComponent}
+        ]},
+    { path: 'login', component: LoginComponent},
+    { path: 'signup', component: SignupComponent}
 ];
+
+// export const routes: Routes = [
+//     { path: '', component: ProductListComponent},
+//     { path: 'category/:query', component: ProductListComponent},
+//     { path: 'product-detail/:id', component: ProductDetailComponent}
+// ];
