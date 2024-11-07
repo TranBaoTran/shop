27d984937348463c563aafda5cdec12653edc400
NightoSlep <jayrisky23@gmail.com>
2024-11-07 12:39:42 +0700

    cart in detail

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product.model';
+import { CartService } from '../../services/cart.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent {
   product: any;
+  inputQuantity: number = 1;
 
-  constructor(private route: ActivatedRoute, private productService : ProductService) {}
+  constructor(private route: ActivatedRoute, private productService : ProductService,
+    private cartService: CartService
+  ) {}
 
   ngOnInit(): void {
     this.getProduct();
@@ -25,4 +31,19 @@ export class ProductDetailComponent {
       }
     })
   } 
+  addToCart(product: Product) {
+      if (this.inputQuantity < 1) {
+        // Kiểm tra số lượng không hợp lệ
+        alert('Số lượng phải lớn hơn hoặc bằng 1');
+        return;
+      }
+      this.cartService.addToCart({ 
+        productId: product.id, 
+        title: product.title, 
+        price: product.price, 
+        image: product.image,
+        quantity: this.inputQuantity
+      });
+      console.log('Sản phẩm đã được thêm vào giỏ hàng', product, this.inputQuantity);
+    }
 }
