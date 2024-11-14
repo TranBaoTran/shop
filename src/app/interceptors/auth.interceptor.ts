46a4be5a1736be911d692d8e885319627b1b570b
TranBaoTran <117694181+TranBaoTran@users.noreply.github.com>
2024-11-15 01:52:31 +0700

    fix small detail

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -4,7 +4,7 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('token');
   const clonedReq = req.clone({
     setHeaders:{
-      Authorization : `Bearer ${token}`
+      'Authorization' : `Bearer ${token}`
     }
   })
   return next(clonedReq);
